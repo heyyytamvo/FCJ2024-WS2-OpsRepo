018d0572108fbdcc121ced6ac7ffd12ad99524a6
Tam Vo <tamvoforwork@gmail.com>
2024-09-11 21:40:43 +0700

    Need to collect pods log

--- Terraform/07-eks.tf
+++ Terraform/07-eks.tf
@@ -1,45 +1,3 @@
-resource "aws_security_group" "Cluster_SG" {
-  name        = "Cluster_SG"
-  description = "Allow HTTP, HTTPS From Jump Host"
-  vpc_id      = aws_vpc.main.id
-
-  ingress = [
-    {
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = []
-      description      = "Allow inbound traffic on port 80"
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = [aws_security_group.EC2_SG.id]
-      self             = false
-    },
-
-    {
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"],
-      description      = "Allow inbound ICMP Traffic"
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = [aws_security_group.EC2_SG.id]
-      self             = false
-    }
-  ]
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "EKS Control Plane SG"
-  }
-}
 module "eks" {
   source                         = "terraform-aws-modules/eks/aws"
   cluster_name                   = var.cluster_name
@@ -50,7 +8,7 @@ module "eks" {
   cluster_endpoint_public_access = true
   iam_role_arn                   = aws_iam_role.ekse_role.arn
   enable_cluster_creator_admin_permissions = true
-  cluster_security_group_id = aws_security_group.Cluster_SG.id
+
   cluster_addons = {
     kube-proxy = {
       most_recent = true

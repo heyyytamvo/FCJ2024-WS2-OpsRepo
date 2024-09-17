5a763f0d6c8a72b2b4bab4b2b80c2ca91cd7c573
Tam Vo <tamvoforwork@gmail.com>
2024-09-17 13:11:07 +0700

    Update Ops

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+#set up argo cd
+kubectl apply -f argocd/namespace.yaml
+kubectl apply -f argocd/default.yaml -n argocd
+
+#set up efk stack
+kubectl apply -f EFK/.
+
+#set up Prometheus and Grafana
 kubectl create namespace monitoring
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install prometheus-operator prometheus-community/kube-prometheus-stack -n monitoring
+kubectl apply -f monitoring/grafana.yaml -n monitoring
 # curl http://prometheus-operator-kube-p-prometheus.monitoring.svc.cluster.local:9090

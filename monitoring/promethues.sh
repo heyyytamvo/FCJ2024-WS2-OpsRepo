e8285197c17085575386228431dea4cedd3eba79
Tam Vo <tamvoforwork@gmail.com>
2024-09-23 14:22:23 +0700

    Update Ops repo

--- monitoring/promethues.sh
+++
@@ -1,5 +0,0 @@
-kubectl create namespace monitoring
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm install prometheus-operator prometheus-community/kube-prometheus-stack -n monitoring
-# curl http://prometheus-operator-kube-p-prometheus.monitoring.svc.cluster.local:9090
